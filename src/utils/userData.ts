6c132f11b968a19dc955289b3d08befaf0fd3dd9
FelipeGeazi <felipegeaziartes@gmail.com>
2023-02-06 12:51:20 -0300

    inserindo dados

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
-  facebookUser: "",
+  nameUser: "Felipe Geazi",
+  githubUser: "FelipeGeazi",
+  linkedinUser: "felipegeazidev",
+  whatsappNumber: "+5521992269944",
+  emailUser: "felipegeaziartes@gmail.com",
+  instagramUser: "lipegeazi/?igshid=YmMyMTA2M2Y=",
+  facebookUser: "felipe.geazi",
 };
